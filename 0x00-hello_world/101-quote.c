83f996f057f9377fe0c72eb88c55abcb15282485
Philo963 <filopaterhany2003@gmail.com>
2023-07-13 14:24:35 +0200

    HELLO WORLD

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
- *
- * Description: print a quote using write function
- * 	Write(int fd.const void *buf.size_t count)
- *
- * Return: 1 (not success)
+ * Return: 1
 */
-
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
