eb10b8099c1b50fe5c32a1bfe9d283f78e269afe
Philo963 <filopaterhany2003@gmail.com>
2023-07-14 15:30:04 +0200

    HELLO WORLD

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of three different digits,
- *	in ascending order, separated by a comma followed by a space.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Description: Prints all possible combinations of three different digits,
+ *		in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0 (success)
 */
 
 int main(void)
